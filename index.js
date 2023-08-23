8e6732794268ecd8f3b9e93e8ae705b914731eeb
Paige <paigehcarroll@gmail.com>
2023-08-22 22:09:19 -0500

    added inquirer require and prompts and then methods

---
+++ index.js
@@ -0,0 +1,32 @@
+const inquirer = require('inquirer');
+
+
+inquirer
+    .prompt([
+        {
+            name: 'text',
+            type: 'input',
+            message: 'Please input up to three characters you would like to be displayed in your logo.'
+        },
+        //is it possible to do an if statement for if they enter more than three characters-not letting the prompt proceed?
+        {
+            name: 'text color',
+            type: 'input',
+            message: 'Please type in a color keyword or a hexadecimal code for the color of your text.'
+        },
+        {
+            name: 'shape',
+            type: 'list',
+            message: 'Please select the shape for your logo from the following choices.',
+            choices: ['Circle', 'Square', 'Triangle']
+        },
+        {
+            name: 'shape color',
+            type: 'input',
+            message: 'Please type in a color keyword or a hexadecimal code for the background color of your chosen shape.',
+
+        }
+    ])
+    .then((response) =>
+        console.log(response)
+    )
